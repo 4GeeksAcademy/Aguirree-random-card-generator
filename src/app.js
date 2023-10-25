13897e30cda45a64f25d4f0db961fabcf1b26d92
juliangoaguirre <143230248+juliangoaguirre@users.noreply.github.com>
2023-10-25 18:28:40 +0000

    finished RCG

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let rank = document.querySelector(".rank");
+  let suits = ["hearts", "diamonds", "spades", "clubs"];
+  let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  function getRandomNumber(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+  card.classList.add(suits[getRandomNumber(suits)]);
+  rank.innerHTML = ranks[getRandomNumber(ranks)];
 };
